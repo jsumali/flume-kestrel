c838651f94e0ce58af3628f8a6d84aa2ecb38fd8
jsumali <j.sumali@gmail.com>
2013-02-26 14:40:53 -0500

    add lib directory with jar dependencies, clean up KestrelSinkjava

--- src/com/blangdon/flume/kestrel/KestrelSink.java
+++ src/com/blangdon/flume/kestrel/KestrelSink.java
@@ -1,6 +1,5 @@
 package com.blangdon.flume.kestrel;
 
-
 import net.rubyeye.xmemcached.MemcachedClient;
 import net.rubyeye.xmemcached.XMemcachedClientBuilder;
 import net.rubyeye.xmemcached.MemcachedClientBuilder;
@@ -24,81 +23,80 @@ import com.google.common.base.Preconditions;
  * Sink that sends events to Kestrel queue
  */
 public class KestrelSink extends EventSink.Base {
-	
 
-    private String queue = null;
-    private String servers = null;
-    private MemcachedClient client = null;
-    private MemcachedClientBuilder builder = null;
+	private String queue = null;
+	private String servers = null;
+	private MemcachedClient client = null;
+	private MemcachedClientBuilder builder = null;
 
-    public KestrelSink(String queue, String servers){
-	//constructor
-	this.queue = queue;
-	this.servers = servers;
+	public KestrelSink(String queue, String servers) {
+		//constructor
+		this.queue = queue;
+		this.servers = servers;
 
-	this.builder = new XMemcachedClientBuilder( AddrUtil.getAddresses(this.servers) );
-	builder.setCommandFactory(new KestrelCommandFactory());
+		this.builder = new XMemcachedClientBuilder(AddrUtil.getAddresses(this.servers));
+		builder.setCommandFactory(new KestrelCommandFactory());
 
-    }
-
-    @Override
-	public void open() throws IOException {
-	// Initialized the sink
-	this.client = this.builder.build();
-    }
-    
-    @Override
-	public void append(Event e) throws IOException {
-	//send to Kestrel
-
-	try{
-	    String message = new String(e.getBody());
-	    this.client.set(this.queue, 0, message);
-	}catch(MemcachedException ex){
-	    throw new IOException("Kestrel Command Failed: " + ex.getMessage());
-	}catch(TimeoutException ex){
-	    throw new IOException("Kestrel Command Timeout: " + ex.getMessage());
-	}catch(InterruptedException ex){
-	    //meh
 	}
-    }
 
+	@Override
+	public void open() throws IOException {
+		// Initialized the sink
+		this.client = this.builder.build();
+	}
 
-    @Override
-	public void close() throws IOException {
-	// Cleanup
-	this.client.shutdown();
-    }
-    
-    public static SinkBuilder builder() {
-	return new SinkBuilder() {
-	    // construct a new parameterized sink
-	    @Override
-		public EventSink build(Context context, String... argv) {
-		Preconditions.checkArgument(argv.length > 1,
-					    "usage: kestrelSink(queueName, server:port, [server2:port, server3:port,...])");
+	@Override
+	public void append(Event e) throws IOException {
+		//send to Kestrel
 
-		String servers = "";
-		for( int i = 1; i < argv.length; ++i ){
-		    if( i > 1 ){
-			servers += " ";
-		    }
-		    servers += argv[i];
+		try {
+			String message = new String(e.getBody());
+			this.client.set(this.queue, 0, message);
+		} catch (MemcachedException ex) {
+			throw new IOException("Kestrel Command Failed: " + ex.getMessage());
+		} catch (TimeoutException ex) {
+			throw new IOException("Kestrel Command Timeout: " + ex.getMessage());
+		} catch (InterruptedException ex) {
+			//meh
 		}
+	}
 
-		return new KestrelSink(argv[0], servers);
-	    }
-	};
-    }
-    
-    /**
-     * This is a special function used by the SourceFactory to pull in this class
-     * as a plugin sink.
-     */
-    public static List<Pair<String, SinkBuilder>> getSinkBuilders() {
-	List<Pair<String, SinkBuilder>> builders =
-	    new ArrayList<Pair<String, SinkBuilder>>();
-	builders.add(new Pair<String, SinkBuilder>("kestrelSink", builder()));
-	return builders;
-    }
+
+	@Override
+	public void close() throws IOException {
+		// Cleanup
+		this.client.shutdown();
+	}
+
+	public static SinkBuilder builder() {
+		return new SinkBuilder() {
+			// construct a new parameterized sink
+			@Override
+			public EventSink build(Context context, String... argv) {
+				Preconditions.checkArgument(argv.length > 1,
+						"usage: kestrelSink(queueName, server:port, [server2:port, server3:port,...])");
+
+				String servers = "";
+				for (int i = 1; i < argv.length; ++i) {
+					if (i > 1) {
+						servers += " ";
+					}
+					servers += argv[i];
+				}
+
+				return new KestrelSink(argv[0], servers);
+			}
+		};
+	}
+
+	/**
+	 * This is a special function used by the SourceFactory to pull in this class
+	 * as a plugin sink.
+	 */
+	public static List<Pair<String, SinkBuilder>> getSinkBuilders() {
+		List<Pair<String, SinkBuilder>> builders =
+				new ArrayList<Pair<String, SinkBuilder>>();
+		builders.add(new Pair<String, SinkBuilder>("kestrelSink", builder()));
+		return builders;
+	}
 }
